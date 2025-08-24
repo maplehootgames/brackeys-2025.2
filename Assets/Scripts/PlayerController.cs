ba559bed9387bb49c16f43b06695e0dc5d306b60
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-08-24 12:08:54 -0400

    Changed pc

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeedModifier;
-    Vector2 wishDir;
+    private moveSpeedModifier;
+    private Vector2 moveDirection;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
-        wishDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveSpeedModifier = 3;
+        moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         transform.Translate(wishDir * Time.deltaTime * moveSpeedModifier);
     }
 }
