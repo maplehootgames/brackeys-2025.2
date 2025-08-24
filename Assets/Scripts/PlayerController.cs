227c64d34ebf60602b49691af8381e2a8d7ed4a9
Liam Morison <liam.jbm2020@gmail.com>
2025-08-24 12:29:58 -0400

    added prefabs and fixed pc

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private moveSpeedModifier;
+    private float moveSpeedModifier;
     private Vector2 moveDirection;
 
     void Update()
     {
         moveSpeedModifier = 3;
         moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        transform.Translate(wishDir * Time.deltaTime * moveSpeedModifier);
+        transform.Translate(moveDirection * Time.deltaTime * moveSpeedModifier);
     }
 }
