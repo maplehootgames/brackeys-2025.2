9997cf5b42b244e1549491cb28ac70d361461e1f
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-08-26 13:48:05 -0400

    Fixed multiple objects not cooking

--- Assets/Scripts/CookStationController.cs
+++ Assets/Scripts/CookStationController.cs
@@ -3,41 +3,13 @@ using UnityEngine;
 public class CookStationController : MonoBehaviour
 {
     bool cookTimerActive;
-    public int cookTimerValue;
+    int cookTimerValue;
     StatusController dishbase;
 
     void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.gameObject.layer == 6)
-        {
-            Debug.Log("trigger enter called");
-
-            if (cookTimerActive == true) {
-                cookTimerValue += 1;
-            }
+        if (collider.gameObject.layer == 6) {
             dishbase = collider.gameObject.GetComponent<StatusController>();
-            cookTimerActive = true;
+            dishbase.cookTimerActive = true;
         }
     }
-    void OnTriggerExit2D(Collider2D collider) {
-        if (collider.gameObject.layer == 6)
-        {
-            Debug.Log("Collision Exit Called");
-            cookTimerValue = 0;
-            cookTimerActive = false;
-        }
-    }
-	void FixedUpdate() {
-		if (cookTimerValue == 350) {
-            dishbase.cookIncrement();
-            cookTimerValue += 1;
-		} else if (cookTimerValue == 700) {
-			dishbase.cookIncrement();
-            cookTimerValue += 1;
-		} else if (cookTimerValue == 1050) {
-            dishbase.burnOut();
-            cookTimerActive = false;
-        } else if (cookTimerActive == true) {
-            cookTimerValue += 1;
-        }
-	}
 }
