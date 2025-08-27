db2a50440ca1e278b20cba03a411476bc5ac0935
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-08-27 11:12:15 -0400

    Review System

---
+++ Assets/Scripts/Customer Scripts/ReviewSystem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ReviewSystem : MonoBehaviour {
+    public int[] reviewList = new int[30];
+    public int averageReview() {
+        oldnum = 0
+        for num in reviewList {
+            oldnum = oldoldnum;
+            oldoldnum + num = oldnum;
+        }
+    }
+}
