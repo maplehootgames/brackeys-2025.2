bf80d94a843be911e8b2a9359904e90df120ef0a
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-08-27 10:38:51 -0400

    fixerd leftover scripts

--- Assets/Scripts/PlayerController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    void Update() {
-        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        transform.Translate(moveDirection * Time.deltaTime * 3);
-    }
-}
