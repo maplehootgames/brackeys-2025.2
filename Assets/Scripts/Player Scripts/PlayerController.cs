0986b46b1abee8bdf15692712a7fb298d69fb83d
ZSmith204 <Zack.a.w.Smith@gmail.com>
2025-08-27 23:48:11 -0400

    added animation to player, ps DO NOT look at playercontroller.cs it is genuanly the worst code i ever wrote

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -2,8 +2,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Update() {
+
+    public Animator animator;
+
+    bool left;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
         Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        transform.Translate(moveDirection * Time.deltaTime * 3);
+        if (left == false)
+        {
+            transform.Translate(moveDirection * Time.deltaTime * 3);
+        }
+        else
+        {
+            transform.Translate(new Vector2(moveDirection.x * -1, moveDirection.y) * Time.deltaTime * 3);
+        }
+
+        if (left)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (moveDirection != Vector2.zero)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            left = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            left = true;
+        }
     }
 }
